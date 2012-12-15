a5bbec143f4218f9cb0b75f17a3da49ef687a25c
Ewoud Smeur <ewoud_smeur@msn.com>
2012-12-14 16:24:56 -0800

    dirty hack for the monarch

--- sw/airborne/subsystems/actuators/motor_mixing.c
+++ sw/airborne/subsystems/actuators/motor_mixing.c
@@ -27,6 +27,7 @@
 
 #include "subsystems/actuators/motor_mixing.h"
 #include "paparazzi.h"
+#include "firmwares/rotorcraft/autopilot.h"
 
 //#include <stdint.h>
 #ifndef INT32_MIN
@@ -65,6 +66,11 @@ static const int32_t roll_coef[MOTOR_MIXING_NB_MOTOR]   = MOTOR_MIXING_ROLL_COEF
 static const int32_t pitch_coef[MOTOR_MIXING_NB_MOTOR]  = MOTOR_MIXING_PITCH_COEF;
 static const int32_t yaw_coef[MOTOR_MIXING_NB_MOTOR]    = MOTOR_MIXING_YAW_COEF;
 static const int32_t thrust_coef[MOTOR_MIXING_NB_MOTOR] = MOTOR_MIXING_THRUST_COEF;
+
+static const int32_t roll_coef2[MOTOR_MIXING_NB_MOTOR]   = MOTOR_MIXING_ROLL_COEF2;
+static const int32_t pitch_coef2[MOTOR_MIXING_NB_MOTOR]  = MOTOR_MIXING_PITCH_COEF2;
+static const int32_t yaw_coef2[MOTOR_MIXING_NB_MOTOR]    = MOTOR_MIXING_YAW_COEF2;
+static const int32_t thrust_coef2[MOTOR_MIXING_NB_MOTOR] = MOTOR_MIXING_THRUST_COEF2;
 
 struct MotorMixing motor_mixing;
 
@@ -149,13 +155,24 @@ void motor_mixing_run(bool_t motors_on, bool_t override_on, pprz_t in_cmd[] ) {
     int32_t max_cmd = INT32_MIN;
     /* do the mixing in float to avoid overflows, implicitly casted back to int32_t */
     for (i=0; i<MOTOR_MIXING_NB_MOTOR; i++) {
-      motor_mixing.commands[i] = MOTOR_MIXING_MIN_MOTOR +
-        (thrust_coef[i] * in_cmd[COMMAND_THRUST] +
-         roll_coef[i]   * in_cmd[COMMAND_ROLL]   +
-         pitch_coef[i]  * in_cmd[COMMAND_PITCH]  +
-         yaw_coef[i]    * in_cmd[COMMAND_YAW]    +
-         motor_mixing.trim[i]) / MOTOR_MIXING_SCALE *
+      if(autopilot_mode == AP_MODE_FORWARD) {
+        motor_mixing.commands[i] = MOTOR_MIXING_MIN_MOTOR +
+        (thrust_coef2[i] * in_cmd[COMMAND_THRUST] +
+        roll_coef2[i]   * in_cmd[COMMAND_ROLL]   +
+        pitch_coef2[i]  * in_cmd[COMMAND_PITCH]  +
+        yaw_coef2[i]    * in_cmd[COMMAND_YAW]    +
+        motor_mixing.trim[i]) / MOTOR_MIXING_SCALE *
         (MOTOR_MIXING_MAX_MOTOR - MOTOR_MIXING_MIN_MOTOR) / MAX_PPRZ;
+      }
+      else {
+        motor_mixing.commands[i] = MOTOR_MIXING_MIN_MOTOR +
+        (thrust_coef[i] * in_cmd[COMMAND_THRUST] +
+        roll_coef[i]   * in_cmd[COMMAND_ROLL]   +
+        pitch_coef[i]  * in_cmd[COMMAND_PITCH]  +
+        yaw_coef[i]    * in_cmd[COMMAND_YAW]    +
+        motor_mixing.trim[i]) / MOTOR_MIXING_SCALE *
+        (MOTOR_MIXING_MAX_MOTOR - MOTOR_MIXING_MIN_MOTOR) / MAX_PPRZ;
+      }
       if (motor_mixing.commands[i] < min_cmd)
         min_cmd = motor_mixing.commands[i];
       if (motor_mixing.commands[i] > max_cmd)
