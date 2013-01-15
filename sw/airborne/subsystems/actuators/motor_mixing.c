f2e520cbc2f2dc6eb94e7d8d7b7ff40ef9ff8b9b
Pranay Sinha <psinha@psinha-laptop.(none)>
2013-01-14 18:02:58 -0800

    multiple motor mixing matrices based on flight mode

--- sw/airborne/subsystems/actuators/motor_mixing.c
+++ sw/airborne/subsystems/actuators/motor_mixing.c
@@ -155,7 +155,7 @@ void motor_mixing_run(bool_t motors_on, bool_t override_on, pprz_t in_cmd[] ) {
     int32_t max_cmd = INT32_MIN;
     /* do the mixing in float to avoid overflows, implicitly casted back to int32_t */
     for (i=0; i<MOTOR_MIXING_NB_MOTOR; i++) {
-      if(autopilot_mode == AP_MODE_FORWARD) {
+      if(autopilot_mode == AP_MODE_RC_DIRECT) {
         motor_mixing.commands[i] = MOTOR_MIXING_MIN_MOTOR +
         (thrust_coef2[i] * in_cmd[COMMAND_THRUST] +
         roll_coef2[i]   * in_cmd[COMMAND_ROLL]   +
