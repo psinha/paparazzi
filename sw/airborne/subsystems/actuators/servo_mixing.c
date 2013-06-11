11f71e76a1aa8e59734e89916877100e08d42b8a
Pranay Sinha <psinha@Sinha-PC>
2013-06-11 21:47:37 +0200

    Debug servo bounds part 2

--- sw/airborne/subsystems/actuators/servo_mixing.c
+++ sw/airborne/subsystems/actuators/servo_mixing.c
@@ -28,8 +28,8 @@
 #include "subsystems/actuators/servo_mixing.h"
 #include "paparazzi.h"
 
-#define SERVO_MIXING_MAX_SERVO 0
-#define SERVO_MIXING_MIN_SERVO -0
+#define SERVO_MIXING_MAX_SERVO MAX_PPRZ/2
+#define SERVO_MIXING_MIN_SERVO -MAX_PPRZ/2
 //#include <stdint.h>
 
 struct ServoMixing servo_mixing;
